2ce556b391847b3b88ad5711794a5e44db2cad7c
Ivan <89821080113@mail.ru>
2021-06-20 17:11:09 +0300

    included TypeScript

--- src/redux/friends-reducer.ts
+++ src/redux/friends-reducer.ts
@@ -4,14 +4,21 @@ import {usersAPI} from "../api/api";
 const SET_FRIENDS = "SET_FRIENDS"
 const SET_TOTAL_FRIENDS_COUNT = "SET_TOTAL_FRIENDS_COUNT"
 
-let initialState = {
+type InitialStateType = {
+    friends: Array<object>
+    totalFriendsCount: number
+    pageSize: number
+    currentPage: number
+}
+
+let initialState: InitialStateType = {
     friends: [],
     totalFriendsCount: 0,
     pageSize: 10,
     currentPage: 1,
 }
 
-const friendsReducer = (state = initialState, action) => {
+const friendsReducer = (state = initialState, action: any) => {
     switch (action.type) {
         case SET_FRIENDS:
             return {...state, friends: action.friends}
@@ -22,10 +29,10 @@ const friendsReducer = (state = initialState, action) => {
     }
 }
 
-export const setFriends = (friends) => ({type: SET_FRIENDS, friends})
-export const setTotalFriendsCount = (count) => ({type: SET_TOTAL_FRIENDS_COUNT, count})
+export const setFriends = (friends: any) => ({type: SET_FRIENDS, friends})
+export const setTotalFriendsCount = (count: any) => ({type: SET_TOTAL_FRIENDS_COUNT, count})
 
-export const getFriends = (page, pageSize,) => async (dispatch) => {
+export const getFriends = (page: any, pageSize: any) => async (dispatch: any) => {
     debugger
     let data = await usersAPI.getFriends(page, pageSize)
     debugger
